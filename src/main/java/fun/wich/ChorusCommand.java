b44f37732e5fc1ed41f25873eab15ca657d6a497
OfficePigeon <ava@wich.fun>
2025-12-04 18:24:51 -0500

    1.15 intial

--- src/main/java/fun/wich/ChorusCommand.java
+++ src/main/java/fun/wich/ChorusCommand.java
@@ -9,12 +9,10 @@ import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.command.arguments.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.FoxEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.math.MathHelper;
@@ -54,13 +52,12 @@ public class ChorusCommand implements ModInitializer {
 			double x = entity.getX(), y = entity.getY(), z = entity.getZ();
 			for (int i = 0; i < 16; ++i) {
 				double g = entity.getX() + (entity.getRandom().nextDouble() - 0.5) * 16;
-				double h = MathHelper.clamp(entity.getY() + (double)(entity.getRandom().nextInt(16) - 8), 0.0F, world.getDimensionHeight() - 1);
+				double h = MathHelper.clamp(entity.getY() + (double)(entity.getRandom().nextInt(16) - 8), 0.0F, world.getEffectiveHeight() - 1);
 				double j = entity.getZ() + (entity.getRandom().nextDouble() - 0.5) * 16;
 				if (entity.hasVehicle()) entity.stopRiding();
 				if (entity.teleport(g, h, j, true)) {
-					SoundEvent soundEvent = entity instanceof FoxEntity ? SoundEvents.ENTITY_FOX_TELEPORT : SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT;
-					world.playSound(null, x, y, z, soundEvent, SoundCategory.PLAYERS, 1, 1);
-					entity.playSound(soundEvent, 1, 1);
+					world.playSound(null, x, y, z, SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1, 1);
+					entity.playSound(SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, 1, 1);
 					break;
 				}
 			}
